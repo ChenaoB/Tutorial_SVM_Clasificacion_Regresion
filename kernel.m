a38f1a992cfb335ec4303a960ea5fb1bd514fca3
ChenaoB <140216998+ChenaoB@users.noreply.github.com>
2024-09-23 15:15:51 -0500

    Delete kernel.m

--- kernel.m
+++
@@ -1,9 +0,0 @@
-function [K] = kernel(X1,X2, lengthScale)
-K = zeros(length(X1), length(X2));
-for d=1:length(X1)
-    for e=1:length(X2)
-        K(d,e) = exp(-lengthScale*((X1(d,:)-X2(e,:))*(X1(d,:)-X2(e,:))'));
-    end
-end
-end
-
